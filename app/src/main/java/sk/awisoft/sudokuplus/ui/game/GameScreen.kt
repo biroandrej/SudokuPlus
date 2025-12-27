e845e17570e5408ccb5ee33de34c646e2e5e4522
biroandrej <info@awisoft.sk>
2025-12-27 16:13:41 +0100

    chore: ktlint fixes

--- app/src/main/java/sk/awisoft/sudokuplus/ui/game/GameScreen.kt
+++ app/src/main/java/sk/awisoft/sudokuplus/ui/game/GameScreen.kt
@@ -24,12 +24,12 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.rounded.AutoAwesome
